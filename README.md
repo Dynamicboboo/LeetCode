04ae6135575f20da63e8e3fe5b584cc49730b63f
牛涛杰 <47921896@qq.com>
2020-08-28 23:55:50 +0800

    最长回文字串

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ leetcode 题解，记录自己的 leetcode 解题之路。
 | :--- | ------------------------------------------------------------ | ------------------------------------------------------------ |
 | 1    | [两数之和](https://leetcode-cn.com/problems/two-sum/)        | [java](https://github.com/Dynamicboboo/LeetCode/blob/master/cn/%5B1%5D%E4%B8%A4%E6%95%B0%E4%B9%8B%E5%92%8C.java) |
 | 2    | [两数相加](https://leetcode-cn.com/problems/add-two-numbers/) | [java]([https://github.com/Dynamicboboo/LeetCode/blob/master/cn/%5B2%5D%E4%B8%A4%E6%95%B0%E7%9B%B8%E5%8A%A0.java](https://github.com/Dynamicboboo/LeetCode/blob/master/cn/[2]两数相加.java)) |
-| 3    |                                                              |                                                              |
-|      |                                                              |                                                              |
+| 3    | [无重复字符的最长字串](https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/) | [java](https://github.com/Dynamicboboo/LeetCode/blob/master/cn/%5B3%5D%E6%97%A0%E9%87%8D%E5%A4%8D%E5%AD%97%E7%AC%A6%E7%9A%84%E6%9C%80%E9%95%BF%E5%AD%90%E4%B8%B2.java) |
+| 5    | [[最长回文子串](https://leetcode-cn.com/problems/longest-palindromic-substring/)] |                                                              |
 |      |                                                              |                                                              |
 |      |                                                              |                                                              |
 |      |                                                              |                                                              |
---
+++ cn/[5]最长回文子串.java
@@ -0,0 +1,71 @@
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。 
+//
+// 示例 1： 
+//
+// 输入: "babad"
+//输出: "bab"
+//注意: "aba" 也是一个有效答案。
+// 
+//
+// 示例 2： 
+//
+// 输入: "cbbd"
+//输出: "bb"
+// 
+// Related Topics 字符串 动态规划 
+// 👍 2619 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        // 特判
+        if (len < 2){
+            return s;
+        }
+
+        int maxLen = 1;//初始最长长度
+        int begin  = 0;
+
+        // 1. 状态定义
+        // dp[i][j] 表示s[i...j] 是否是回文串
+        // 2. 初始化
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] chars = s.toCharArray();//将字符串转换为字符数组
+        // 3. 状态转移
+        // 注意：先填左下角
+        // 填表规则：先一列一列的填写，再一行一行的填，保证左下方的单元格先进行计算
+        for (int j = 1;j < len;j++){
+            for (int i = 0; i < j; i++) {
+                // 头尾字符不相等，不是回文串
+                if (chars[i] != chars[j]){
+                    dp[i][j] = false;
+                }else {
+                    // 相等的情况下
+                    // 考虑头尾去掉以后没有字符剩余，或者剩下一个字符的时候，肯定是回文串
+                    if (j - i < 3){
+                        dp[i][j] = true;
+                    }else {
+                        // 状态转移
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要dp[i][j] == true 成立，表示s[i...j] 是否是回文串
+                // 此时更新记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen){
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        // 4. 返回值
+        return s.substring(begin,begin + maxLen);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
