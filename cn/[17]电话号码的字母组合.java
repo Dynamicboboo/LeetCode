d48db36dc4fc375904496d27025dc75c85dafeb3
牛涛杰 <47921896@qq.com>
2020-09-17 21:16:03 +0800

    17电话号码的字母组合

---
+++ cn/[17]电话号码的字母组合.java
@@ -0,0 +1,50 @@
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。 
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。 
+//
+// 
+//
+// 示例: 
+//
+// 输入："23"
+//输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+// 
+//
+// 说明: 
+//尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。 
+// Related Topics 字符串 回溯算法 
+// 👍 919 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits==null || digits.length()==0) {
+            return new ArrayList<String>();
+        }
+        //一个映射表，第二个位置是"abc“,第三个位置是"def"。。。
+        //这里也可以用map，用数组可以更节省点内存
+        String[] letter_map = {
+                " ","*","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+        };
+        List<String> res = new ArrayList<>();
+        //先往队列中加入一个空字符
+        res.add("");
+        for(int i=0;i<digits.length();i++) {
+            //由当前遍历到的字符，取字典表中查找对应的字符串
+            String letters = letter_map[digits.charAt(i)-'0'];
+            int size = res.size();
+            //计算出队列长度后，将队列中的每个元素挨个拿出来
+            for(int j=0;j<size;j++) {
+                //每次都从队列中拿出第一个元素
+                String tmp = res.remove(0);
+                //然后跟"def"这样的字符串拼接，并再次放到队列中
+                for(int k=0;k<letters.length();k++) {
+                    res.add(tmp+letters.charAt(k));
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
