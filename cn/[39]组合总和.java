0bd89fdb110e7820d51ac3d183ff09ffd69899bd
牛涛杰 <47921896@qq.com>
2020-10-08 17:34:19 +0800

    39组合总算合

--- cn/[39]组合总和.java
+++ cn/[39]组合总和.java
@@ -43,10 +43,38 @@
 // 👍 981 👎 0
 
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) return res;
+        Arrays.sort(candidates);
+        Deque<Integer> path = new ArrayDeque<>();
+
+        dfs(candidates,0,len,target, path,  res);
+        return res;
+    }
+    public void dfs(int[] candidates,int begin, int len ,int target,Deque<Integer> path,List<List<Integer>> res ){
+        if (target == 0){
+            res.add(new ArrayList<>(path));
+            return ;
+        }
+        for (int i = begin; i < len ; i++) {
+            if (target - candidates[i] < 0){
+                break;
+            }
+            path.addLast(candidates[i]);
+            dfs(candidates,i,len,target-candidates[i],path,res);
+            path.removeLast();
+        }
 
     }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
